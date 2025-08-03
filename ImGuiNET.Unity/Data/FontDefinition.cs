e027a7dcce0b3f2af4d92f345c8da3c61ea3e5d7
Stefan Tudoran <stefantudoran@gmail.com>
2025-08-04 02:27:00 +0300

    fhgdfghgfhfgf

---
+++ ImGuiNET.Unity/Data/FontDefinition.cs
@@ -0,0 +1,14 @@
+#if IMGUI_DEBUG || UNITY_EDITOR
+using UnityEngine;
+
+namespace ImGuiNET.Unity
+{
+    [System.Serializable]
+    struct FontDefinition
+    {
+        [SerializeField] Object _fontAsset; // to drag'n'drop file from the inspector
+        public string FontPath;
+        public FontConfig Config;
+    }
+}
+#endif
---
+++ ImGuiNET.Unity/Platform/IImGuiPlatform.cs
@@ -0,0 +1,16 @@
+﻿#if IMGUI_DEBUG || UNITY_EDITOR
+using UnityEngine;
+
+namespace ImGuiNET.Unity
+{
+    /// <summary>
+    /// Platform bindings for ImGui in Unity in charge of: mouse/keyboard/gamepad inputs, cursor shape, timing, windowing.
+    /// </summary>
+    interface IImGuiPlatform
+    {
+        bool Initialize(ImGuiIOPtr io);
+        void Shutdown(ImGuiIOPtr io);
+        void PrepareFrame(ImGuiIOPtr io, Rect displayRect);
+    }
+}
+#endif
